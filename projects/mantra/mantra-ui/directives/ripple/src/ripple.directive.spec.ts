927eac8bdb4e329c72d08c853adcb109b044d958
rubfermarz <ruben@rubenincode.tech>
2024-02-03 21:32:09 +0100

    Added basic ripple

---
+++ projects/mantra/mantra-ui/directives/ripple/src/ripple.directive.spec.ts
@@ -0,0 +1,11 @@
+import { RippleDirective } from './ripple.directive';
+import { DebugElement } from '@angular/core';
+
+describe('RippleDirective', () => {
+  let element: DebugElement;
+
+  it('should create an instance', () => {
+    const directive = new RippleDirective(element);
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MantraButton } from '@mantra/mantra-ui/components/button';
+import { RippleDirective } from '@mantra/mantra-ui/directives/ripple';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MantraButton],
+  imports: [RouterOutlet, MantraButton, RippleDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
