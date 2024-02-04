265ed22c8644d5f70316b7fce26005c58eff7396
rubfermarz <ruben@rubenincode.tech>
2024-02-04 11:26:17 +0100

    Added prettier file config with some rules

--- projects/mantra/mantra-ui/directives/ripple/src/ripple.directive.ts
+++ projects/mantra/mantra-ui/directives/ripple/src/ripple.directive.ts
@@ -1,41 +1,42 @@
 import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[mantraRipple]',
-  standalone: true,
+    selector: '[mantraRipple]',
+    standalone: true,
 })
 export class RippleDirective implements OnInit {
-  constructor(protected elementRef: ElementRef) {
-    this.elementRef = elementRef;
-  }
-
-  ngOnInit() {
-    console.log('RippleDirective');
-  }
-
-  @HostListener('click', ['$event']) onClick($event: MouseEvent) {
-    this.createRipple($event);
-  }
-
-  private createRipple($event: MouseEvent) {
-    const targetElement = this.elementRef.nativeElement;
-    const rippleElement: HTMLSpanElement = document.createElement('span');
-    const diameter = Math.max(
-      targetElement.clientWidth,
-      targetElement.clientHeight,
-    );
-    const radius = diameter / 2;
-
-    rippleElement.style.width = rippleElement.style.height = `${diameter}px`;
-    rippleElement.style.left = `${$event.clientX - (targetElement.offsetLeft + radius)}px`;
-    rippleElement.style.top = `${$event.clientY - (targetElement.offsetTop + radius)}px`;
-    rippleElement.classList.add('ripple');
-
-    const ripple = targetElement.getElementsByClassName('ripple')[0];
-    if (ripple) {
-      ripple.remove();
+    constructor(protected elementRef: ElementRef) {
+        this.elementRef = elementRef;
     }
 
-    targetElement.appendChild(rippleElement);
-  }
+    ngOnInit() {
+        console.log('RippleDirective');
+    }
+
+    @HostListener('click', ['$event']) onClick($event: MouseEvent) {
+        this.createRipple($event);
+    }
+
+    private createRipple($event: MouseEvent) {
+        const targetElement = this.elementRef.nativeElement;
+        const rippleElement: HTMLSpanElement = document.createElement('span');
+        const diameter = Math.max(
+            targetElement.clientWidth,
+            targetElement.clientHeight
+        );
+        const radius = diameter / 2;
+
+        rippleElement.style.width =
+            rippleElement.style.height = `${diameter}px`;
+        rippleElement.style.left = `${$event.clientX - (targetElement.offsetLeft + radius)}px`;
+        rippleElement.style.top = `${$event.clientY - (targetElement.offsetTop + radius)}px`;
+        rippleElement.classList.add('ripple');
+
+        const ripple = targetElement.getElementsByClassName('ripple')[0];
+        if (ripple) {
+            ripple.remove();
+        }
+
+        targetElement.appendChild(rippleElement);
+    }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { MantraButton } from '@mantra/mantra-ui/components/button';
 import { RippleDirective } from '@mantra/mantra-ui/directives/ripple';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MantraButton, RippleDirective],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, MantraButton, RippleDirective],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mantra';
+    title = 'mantra';
 }
