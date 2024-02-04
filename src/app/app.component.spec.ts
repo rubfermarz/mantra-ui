bd5f09a21b0eadb175575c093d2598370a558954
rubfermarz <ruben@rubenincode.tech>
2024-02-04 12:23:59 +0100

    Revert "Added linter angular specific"
    This reverts commit 3d731ad1a8a89dde44b6633238e9963165ab1cce.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it("should have the 'mantra' title", () => {
+    it('should have the \'mantra\' title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual('mantra');
